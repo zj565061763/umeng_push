dea2abfd8afade184c57717bc41fb8dc18dd0fba
zjun <565061763@qq.com>
2021-06-26 12:01:18 +0800

    lib

--- app/src/main/java/com/sd/demo/umeng_push/MainActivity.java
+++ app/src/main/java/com/sd/demo/umeng_push/MainActivity.java
@@ -1,12 +1,11 @@
 package com.sd.demo.umeng_push;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity
 {
-
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
