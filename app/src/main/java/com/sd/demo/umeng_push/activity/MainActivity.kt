36fca49553f44a68ebb137df9bb44f2dbffa5680
zjun <565061763@qq.com>
2021-06-28 12:02:10 +0800

    demo

--- app/src/main/java/com/sd/demo/umeng_push/activity/MainActivity.kt
+++ app/src/main/java/com/sd/demo/umeng_push/activity/MainActivity.kt
@@ -1,15 +1,33 @@
 package com.sd.demo.umeng_push.activity
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import anet.channel.util.Utils.context
 import com.sd.demo.umeng_push.R
 import com.umeng.commonsdk.UMConfigure
+import com.umeng.message.IUmengRegisterCallback
+import com.umeng.message.PushAgent
+
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = MainActivity::class.java.simpleName
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         initUmengSDK()
+
+        val pushAgent = PushAgent.getInstance(context)
+        pushAgent.register(object : IUmengRegisterCallback {
+            override fun onSuccess(deviceToken: String) {
+                Log.i(TAG, "onSuccess ${deviceToken}")
+            }
+
+            override fun onFailure(code: String, desc: String) {
+                Log.e(TAG, "onFailure ${code}  ${desc}")
+            }
+        })
     }
 
     private fun initUmengSDK() {
