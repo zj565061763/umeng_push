a8e4d24d27d0882388b32398c3006c3c8c59a322
zjun <565061763@qq.com>
2021-06-28 11:55:31 +0800

    refactor

--- app/src/main/java/com/sd/demo/umeng_push/MainActivity.kt
+++ app/src/main/java/com/sd/demo/umeng_push/MainActivity.kt
@@ -1,15 +1,11 @@
-package com.sd.demo.umeng_push;
+package com.sd.demo.umeng_push
 
-import android.os.Bundle;
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-import androidx.appcompat.app.AppCompatActivity;
-
-public class MainActivity extends AppCompatActivity
-{
-    @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
     }
 }
