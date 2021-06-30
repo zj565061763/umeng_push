e8f7cb9f229ba09e3c5cab7d5d9fa399bdf18b02
zjun <565061763@qq.com>
2021-06-30 09:22:32 +0800

    demo

--- app/src/main/java/com/sd/demo/umeng_push/activity/MainActivity.kt
+++ app/src/main/java/com/sd/demo/umeng_push/activity/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.sd.demo.umeng_push.activity
 
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import anet.channel.util.Utils.context
 import com.sd.demo.umeng_push.R
 import com.umeng.commonsdk.UMConfigure
 import com.umeng.message.IUmengRegisterCallback
 import com.umeng.message.PushAgent
+import com.umeng.message.UmengMessageHandler
+import com.umeng.message.UmengNotificationClickHandler
+import com.umeng.message.entity.UMessage
 
 
 class MainActivity : AppCompatActivity() {
@@ -17,8 +20,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         initUmengSDK()
+    }
 
-        val pushAgent = PushAgent.getInstance(context)
+    private fun initUmengSDK() {
+        UMConfigure.init(
+            this, "60d28f6326a57f101832de50", "Umeng",
+            UMConfigure.DEVICE_TYPE_PHONE,
+            "be29515ba1416294e6103410bb1eaad3"
+        )
+
+        val pushAgent = PushAgent.getInstance(this)
+        pushAgent.apply {
+            this.notificationClickHandler = _notificationClickHandler
+            this.messageHandler = _umengMessageHandler
+        }
         pushAgent.register(object : IUmengRegisterCallback {
             override fun onSuccess(deviceToken: String) {
                 Log.i(TAG, "onSuccess ${deviceToken}")
@@ -30,11 +45,36 @@ class MainActivity : AppCompatActivity() {
         })
     }
 
-    private fun initUmengSDK() {
-        UMConfigure.init(
-            this, "60d28f6326a57f101832de50", "Umeng",
-            UMConfigure.DEVICE_TYPE_PHONE,
-            "be29515ba1416294e6103410bb1eaad3"
-        )
+    private val _umengMessageHandler = object : UmengMessageHandler() {
+        override fun dealWithNotificationMessage(context: Context, msg: UMessage) {
+            super.dealWithNotificationMessage(context, msg)
+            Log.i(TAG, "dealWithNotificationMessage")
+        }
+
+        override fun dealWithCustomMessage(context: Context, msg: UMessage) {
+            super.dealWithCustomMessage(context, msg)
+            Log.i(TAG, "dealWithCustomMessage")
+        }
+    }
+
+    private val _notificationClickHandler: UmengNotificationClickHandler = object : UmengNotificationClickHandler() {
+        override fun launchApp(context: Context, msg: UMessage) {
+            super.launchApp(context, msg)
+            Log.i(TAG, "launchApp")
+        }
+
+        override fun openUrl(context: Context, msg: UMessage) {
+            super.openUrl(context, msg)
+            Log.i(TAG, "openUrl")
+        }
+
+        override fun openActivity(context: Context, msg: UMessage) {
+            super.openActivity(context, msg)
+            Log.i(TAG, "openActivity")
+        }
+
+        override fun dealWithCustomAction(context: Context, msg: UMessage) {
+            Log.i(TAG, "dealWithCustomAction ${msg.custom}")
+        }
     }
 }
