00d66be628146b332b5f6239756f99be55fbb969
zjun <565061763@qq.com>
2021-06-30 15:05:38 +0800

    厂商通道 华为

--- lib/src/main/java/com/sd/lib/umeng_push/LibUmengPush.java
+++ lib/src/main/java/com/sd/lib/umeng_push/LibUmengPush.java
@@ -1,9 +1,11 @@
 package com.sd.lib.umeng_push;
 
+import android.app.Application;
 import android.content.Context;
 import android.content.res.Resources;
 import android.text.TextUtils;
 
+import org.android.agoo.huawei.HuaWeiRegister;
 import org.android.agoo.xiaomi.MiPushRegistar;
 
 public class LibUmengPush {
@@ -17,6 +19,11 @@ public class LibUmengPush {
         final String xiaomiAppKey = resources.getString(R.string.lib_umeng_push_xiaomi_app_key);
         if (!TextUtils.isEmpty(xiaomiAppId) && !TextUtils.isEmpty(xiaomiAppKey)) {
             MiPushRegistar.register(context, xiaomiAppId, xiaomiAppKey);
+        }
+
+        final String huaweiAppId = resources.getString(R.string.lib_umeng_push_huawei_app_id);
+        if (!TextUtils.isEmpty(huaweiAppId)) {
+            HuaWeiRegister.register((Application) context.getApplicationContext());
         }
     }
 }
