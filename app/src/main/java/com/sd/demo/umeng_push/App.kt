bbf86c57a0f424d8b183f5781281120cf64bdc94
zjun <565061763@qq.com>
2021-06-30 15:59:19 +0800

    multidex

--- app/src/main/java/com/sd/demo/umeng_push/App.kt
+++ app/src/main/java/com/sd/demo/umeng_push/App.kt
@@ -1,11 +1,18 @@
 package com.sd.demo.umeng_push
 
 import android.app.Application
+import android.content.Context
+import androidx.multidex.MultiDex
 import com.umeng.commonsdk.UMConfigure
 
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
         UMConfigure.preInit(this, "60d28f6326a57f101832de50", "umeng")
+    }
+
+    override fun attachBaseContext(base: Context?) {
+        super.attachBaseContext(base)
+        MultiDex.install(this)
     }
 }
