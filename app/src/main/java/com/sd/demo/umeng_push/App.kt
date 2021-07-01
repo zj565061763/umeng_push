72aa71b0d62b2be36bced57ee8b21bb0aa87b324
zjun <565061763@qq.com>
2021-07-01 17:01:49 +0800

    UMConfigure.setLogEnabled(true)

--- app/src/main/java/com/sd/demo/umeng_push/App.kt
+++ app/src/main/java/com/sd/demo/umeng_push/App.kt
@@ -8,6 +8,7 @@ import com.umeng.commonsdk.UMConfigure
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
+        UMConfigure.setLogEnabled(true)
         UMConfigure.preInit(this, "60d28f6326a57f101832de50", "umeng")
     }
 
