04fcb4a61d952b2210521f90be48b13cbae216bf
zjun <565061763@qq.com>
2021-07-02 10:23:45 +0800

    umeng_common

--- app/src/main/java/com/sd/demo/umeng_push/App.kt
+++ app/src/main/java/com/sd/demo/umeng_push/App.kt
@@ -3,16 +3,15 @@ package com.sd.demo.umeng_push
 import android.app.Application
 import android.content.Context
 import androidx.multidex.MultiDex
+import com.sd.lib.umeng_common.LibUmengCommon
 import com.sd.lib.umeng_push.LibUmengPush
 import com.umeng.commonsdk.UMConfigure
 
 class App : Application() {
-    private val TAG = "App"
-
     override fun onCreate() {
         super.onCreate()
         UMConfigure.setLogEnabled(true)
-        UMConfigure.preInit(this, "60d28f6326a57f101832de50", "umeng")
+        LibUmengCommon.preInit(this)
         LibUmengPush.preRegister(this)
     }
 
--- app/src/main/java/com/sd/demo/umeng_push/activity/MainActivity.kt
+++ app/src/main/java/com/sd/demo/umeng_push/activity/MainActivity.kt
@@ -5,8 +5,8 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.sd.demo.umeng_push.R
+import com.sd.lib.umeng_common.LibUmengCommon
 import com.sd.lib.umeng_push.LibUmengPush
-import com.umeng.commonsdk.UMConfigure
 import com.umeng.message.IUmengRegisterCallback
 import com.umeng.message.PushAgent
 import com.umeng.message.UmengMessageHandler
@@ -24,13 +24,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initUmengSDK() {
-        UMConfigure.init(
-                this,
-                "60d28f6326a57f101832de50",
-                "Umeng",
-                UMConfigure.DEVICE_TYPE_PHONE,
-                "be29515ba1416294e6103410bb1eaad3"
-        )
+        LibUmengCommon.init(this)
 
         val pushAgent = PushAgent.getInstance(this)
         pushAgent.apply {
