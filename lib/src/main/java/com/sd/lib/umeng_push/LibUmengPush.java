35ee7ebfb1cf03230cda081de543a77744c90a9b
zjun <565061763@qq.com>
2021-07-27 15:54:52 +0800

    LibUmengPush

--- lib/src/main/java/com/sd/lib/umeng_push/LibUmengPush.java
+++ lib/src/main/java/com/sd/lib/umeng_push/LibUmengPush.java
@@ -1,24 +1,47 @@
 package com.sd.lib.umeng_push;
 
+import android.app.ActivityManager;
 import android.content.Context;
+import android.text.TextUtils;
 
-import com.umeng.commonsdk.utils.UMUtils;
 import com.umeng.message.IUmengRegisterCallback;
 import com.umeng.message.PushAgent;
+
+import java.util.List;
 
 public class LibUmengPush {
     private LibUmengPush() {
     }
 
     public static void registerOtherProcess(Context context) {
-        if (!UMUtils.isMainProgress(context)) {
+        final String processName = getProcessName(context);
+        final String pushProcess = context.getPackageName() + ":channel";
+        if (TextUtils.equals(processName, pushProcess)) {
             PushAgent.getInstance(context).register(null);
         }
     }
 
     public static void registerMainProcess(Context context, IUmengRegisterCallback callback) {
-        if (UMUtils.isMainProgress(context)) {
+        final String processName = getProcessName(context);
+        final String mainProcess = context.getPackageName();
+        if (TextUtils.equals(processName, mainProcess)) {
             PushAgent.getInstance(context).register(callback);
         }
+    }
+
+    private static String getProcessName(Context context) {
+        final ActivityManager manager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        final List<ActivityManager.RunningAppProcessInfo> list = manager.getRunningAppProcesses();
+        if (list == null || list.isEmpty()) {
+            return null;
+        }
+
+        final int pid = android.os.Process.myPid();
+        for (ActivityManager.RunningAppProcessInfo item : list) {
+            if (item.pid == pid) {
+                return item.processName;
+            }
+        }
+        return null;
     }
 }
